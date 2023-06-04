c2d6813a922d6ca9f8bb04d26e5e1852b2292d59
Klaudia Irena Koch <klkoch@student.wszib.edu.pl>
2023-06-04 18:03:30 +0200

    Długo wyczekiwany Feature: teraz wykrywamy pitagorejskie

--- trojkaty.java
+++ trojkaty.java
@@ -9,10 +9,22 @@ class Trojkaty {
      * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
+	public static boolean czyProstokatny(float a, float b, float c){
+                   if (a * a + b * b == c*c) return true;
+                   if (b * b + c * c == a*a) return true;
+                   if (a * a + c * c == b*b) return true;
+                   return false;
+               }
     public static void jakiTrojkat(float a, float b, float c){
          if (a == b && b == c && a == c) {
              System.out.println("Trójkąt równoboczny"); 
          }
+	if (a == b || b == c || a == c) {
+                    System.out.println("Trójkąt równoramienny");
+         }
+	if (czyProstokatny(a, b, c)) {
+                    System.out.println("Trójkąt prostokątny");
+                  }
          // TODO: tutaj trzeba bedzie dopisac inne przypadki
     }
     /** Wyświetla ekran pomocy */
